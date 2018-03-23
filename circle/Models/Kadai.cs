32e179490ce14a0429ca5e7608355663bb6b6cb8
keitokusano <keitokusano5656@gmail.com>
2018-03-23 22:35:03 +0900

    課題管理画面追加

---
+++ circle/Models/Kadai.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace circle.Models
+{
+    public class Kadai
+    {
+        public Kadai()
+        {
+        }
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
